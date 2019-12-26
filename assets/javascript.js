b465774f0f909ca56ad07da0937c9c6d12105fc4
John Chen <galearii@gmail.com>
2019-12-26 13:54:08 +0800

    Initial build of desktop menu

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,5 +1,4 @@
 $( document ).ready(function() {
-  let win$            = $(window);
   let hamburger_icon$ = $('.menu-icon-hamburger');
   let close_icon$     = $('.menu-icon-close');
   let mobile_menu$    = $('.menu-mobile');
@@ -17,14 +16,5 @@ $( document ).ready(function() {
     mobile_menu$.toggle();
     close_icon$.toggle();
     hamburger_icon$.toggle();
-  });
-
-  win$.resize(function(e) {
-    if(win$.width() >= 768) {
-      icon_container$.hide();
-    }
-    else {
-      icon_container$.show();
-    }
   });
 });
