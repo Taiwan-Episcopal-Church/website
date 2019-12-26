33abf7a982977b2e782445794d5caa78d8e0ee84
John Chen <galearii@gmail.com>
2019-12-26 14:24:39 +0800

    Removed mobile menu in desktop view through JS

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+  let win$            = $(window);
   let hamburger_icon$ = $('.menu-icon-hamburger');
   let close_icon$     = $('.menu-icon-close');
   let mobile_menu$    = $('.menu-mobile');
@@ -6,15 +7,28 @@ $( document ).ready(function() {
 
   hamburger_icon$.click(function(e) {
     e.preventDefault();
-    mobile_menu$.toggle();
-    hamburger_icon$.toggle();
-    close_icon$.toggle();
+    mobile_menu$.show();
+    hamburger_icon$.hide();
+    close_icon$.show();
   });
 
   close_icon$.click(function(e) {
     e.preventDefault();
-    mobile_menu$.toggle();
-    close_icon$.toggle();
-    hamburger_icon$.toggle();
+    mobile_menu$.hide();
+    close_icon$.hide();
+    hamburger_icon$.show();
+  });
+
+  win$.resize(function(e) {
+    if(win$.width() >= 768) {
+      icon_container$.hide();
+      mobile_menu$.hide();
+    }
+    else {
+      icon_container$.show();
+      hamburger_icon$.show();
+      close_icon$.hide();
+      mobile_menu$.hide();
+    }
   });
 });
